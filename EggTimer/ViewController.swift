1955259903bffef7a6b3f0ed1cb7096e90100caf
Aleksandr F <a7214370@gmail.com>
2022-07-08 21:58:13 +0300

    Setup ViewController

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        print(sender.currentTitle)
     }
-
-
+    
 }
 
